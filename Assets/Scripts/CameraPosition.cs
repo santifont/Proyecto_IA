bf83635ee3034889fa2fc366070e0e4a87978f64
santifont <sanalffer@alu.edu.gva.es>
2026-01-15 12:00:23 +0100

    Queda programar puntaje y escena de GAMEOVER

---
+++ Assets/Scripts/CameraPosition.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour
+{
+    private GameObject chompMan;
+    private float posZ;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        chompMan = GameObject.Find("Chomp");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 14 ~ 28 Z
+        posZ = chompMan.transform.position.z;
+        transform.position = new Vector3(0.0f, 35.0f, posZ);
+
+        if (transform.position.z < 14.0f)
+        {
+            transform.position = new Vector3(0.0f, 35.0f, 14.0f);
+        }
+
+        if (transform.position.z > 28.0f)
+        {
+            transform.position = new Vector3(0.0f, 35.0f, 28.0f);
+        }
+    }
+}
