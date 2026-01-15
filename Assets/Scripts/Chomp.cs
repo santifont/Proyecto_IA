0f7c40eaadf7fd9d4d09d2fe9fd93cf96c265277
santifont <sanalffer@alu.edu.gva.es>
2026-01-15 10:11:27 +0100

    InputActions Implementado

--- Assets/Scripts/Chomp.cs
+++ Assets/Scripts/Chomp.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class Chomp : MonoBehaviour
 {
+    private Vector2 myMove;
+    private float characterSpeed = 5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +16,39 @@ public class Chomp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * myMove *  characterSpeed * Time.deltaTime);
+    }
+
+    public void MovementCallback(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started)
+        {
+        }
+
+        if (ctx.performed)
+        {
+            myMove = ctx.ReadValue<Vector2>();
+            Debug.Log(myMove);
+        }
+
+        if (ctx.canceled)
+        {
+            myMove = ctx.ReadValue<Vector2>();
+            Debug.Log(myMove);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("ball"))
+        {
+            Debug.Log("BALL");
+            Destroy(other);
+        }
+
+        if (other.CompareTag("cherry"))
+        {
+            Debug.Log("You touched a CHERRY");
+        }
     }
 }
---
+++ Assets/Scripts/SpawnBalls.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class SpawnBalls : MonoBehaviour
+{
+    private GameObject[] spawn;
+    public GameObject ball;
+    public GameObject powerUp;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spawn = GameObject.FindGameObjectsWithTag("spawnpoint");
+
+        for (int i = 0; i < spawn.Length; i++)
+        {
+            Instantiate(ball, spawn[i].transform.position, Quaternion.identity);
+            Destroy(spawn[i]);
+        }
+
+        Debug.Log(spawn.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
