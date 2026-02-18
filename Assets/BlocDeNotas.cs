d4fabe7f8ea843802301d0234eb2129437f6900e
santifont <sanalffer@alu.edu.gva.es>
2026-02-18 09:56:28 +0100

    Plugin TMP Effects
    He añadido un plugin para animar texto en el juego.

---
+++ Assets/BlocDeNotas.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class BlocDeNotas : MonoBehaviour
+{
+    /*
+     Enlace al plugin de texto animado:
+    https://github.com/Luca3317/TMPEffects
+     
+     
+     
+     
+     
+     */
+}
